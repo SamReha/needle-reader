daba558594d96454f43620e6c6449469f0581c20
Sam Reha <samreha@eduroam-bowers-128-114-154-176.ucsc.edu>
2017-07-27 13:09:23 -0700

    Finished code for now, need to get relays + 12v ps to make lights work

--- needle_reader.ino
+++ needle_reader.ino
@@ -1,17 +1,31 @@
-#define NEEDLE_IN A0
+/* 
+  Pin Configuration
+*/
+#define RED_NEEDLE_IN A0
 #define WHITE_NEEDLE_OUT 9
+#define RED_LIGHT 2
+#define ORANGE_LIGHT 3
+#define SWITCH_IN A1
 
+// Demo vars
 int needleSetting = 0;
 long upTime;
 
 void setup() {
   Serial.begin(9600);
   upTime = millis();
+
+  pinMode(RED_LIGHT, OUTPUT);
+  pinMode(ORANGE_LIGHT, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(voltToDial(analogRead(NEEDLE_IN)));
+  // Report red needle value and switch state
+  Serial.print(readRedNeedle());
+  Serial.print(", ");
+  //Serial.println(getSwitchState());
+  Serial.println(getSwitchState());
+  
   setWhiteNeedle(needleSetting);
 
   if (millis() - upTime >= 1000) {
@@ -20,6 +34,11 @@ void loop() {
   }
 }
 
+float readRedNeedle() {
+  return voltToDial(analogRead(RED_NEEDLE_IN));
+}
+
+// Converts voltage from analogRead to dial setting (for red needle)
 float voltToDial(int volt) {
   volt = constrain(volt, 25, 700);
 
@@ -27,9 +46,22 @@ float voltToDial(int volt) {
   return (volt - 25) * (31 / 630.0);
 }
 
-float setWhiteNeedle(float dialSetting) {
+void setWhiteNeedle(float dialSetting) {
   // Map: 0 - 30 dial setting == 0 - 155 output
   
   analogWrite(WHITE_NEEDLE_OUT, dialSetting * (155 / 30.0));
+}
+
+bool getSwitchState() {
+  return analogRead(SWITCH_IN) > 650;
+}
+
+// Hmm... I think I need to work out a way to step up the voltage for these lights.
+void lightOrange() {
+  digitalWrite(HIGH, ORANGE_LIGHT);
+}
+
+void lightRed() {
+  digitalWrite(HIGH, RED_LIGHT);
 }
 
